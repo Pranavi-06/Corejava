878c88ad219b3ee14e96c70e2e40abf945664ffb
Pranavi-06 <pranavisakamuri@gmail.com>
2025-04-03 08:59:04 +0530

    Update Assessment.java

--- Assessment.java
+++ Assessment.java
@@ -324,43 +324,33 @@ public class BankingSystem {
 }
 //Q6: Secure User Data with Encapsulation
 import java.util.Scanner;
-
 class User {
     private String name;
     private String password;
-
     public void setName(String name) {
         this.name = name;
     }
-
     public String getName() {
         return name;
     }
-
     public void setPassword(String password) {
         this.password = password;
     }
-
     public boolean validatePassword(String input) {
         return password.equals(input);
     }
 }
-
 public class setPassword {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         User user = new User();
-
         System.out.println("Enter your name:");
         String name = scanner.nextLine();
         user.setName(name);
-
         System.out.println("Set your password:");
         String password = scanner.nextLine();
         user.setPassword(password);
-
         System.out.println("\nName: " + user.getName());
-
         System.out.println("Enter your password for validation:");
         String inputPassword = scanner.nextLine();
         if (user.validatePassword(inputPassword)) {
